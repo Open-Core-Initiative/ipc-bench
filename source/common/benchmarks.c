8a0cc97505df4412e91ba9e72720aecb8258c748
Prajval Raval <ravalprajval@gmail.com>
2024-01-11 00:57:31 +0530

    dt3

--- source/common/benchmarks.c
+++ source/common/benchmarks.c
@@ -53,8 +53,10 @@ void evaluate(Benchmarks* bench, Arguments* args) {
 	sigma = sqrt(sigma - (average * average));
 
 	const double tt = ((double)total_time/1000000000);
-	const double messageRate = ((double)((args->count * args->size) * 8) / tt);
+	const double argsTT = ((double)((args->count * args->size) * 8));
+	const double messageRate = ((double)((args->count * args->size) * 8) / (double)tt);
 
+	printf("args %.7f\n", argsTT);
 	printf("tt  %.7f\n", tt);
 	printf("messageRate  %.7f\n", messageRate);
 
