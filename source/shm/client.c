49bb707d12ac4c0d22374d370c655a0bd2fe1b22
Prajval Raval <ravalprajval@gmail.com>
2024-01-12 00:55:05 +0530

    separate packet

--- source/shm/client.c
+++ source/shm/client.c
@@ -22,58 +22,83 @@
 #include "common/sockets.h"
 #include "common/tuntcp.h"
 
-void cleanup(char* shared_memory) {
+void cleanup(char *shared_memory)
+{
 	shmdt(shared_memory);
 }
 
-void cleanup_tcp(int descriptor, void* buffer) {
+void cleanup_tcp(int descriptor, void *buffer)
+{
 	close(descriptor);
 	free(buffer);
 }
 
-void shm_wait(atomic_char* guard) {
+void shm_wait(atomic_char *guard)
+{
 	while (atomic_load(guard) != 'c')
 		;
 }
 
-void shm_notify(atomic_char* guard) {
+void shm_notify(atomic_char *guard)
+{
 	atomic_store(guard, 's');
 }
 
 void communicate(int descriptor,
-								 char* shared_memory,
-								 struct Arguments* args,
-								 struct tcp_conn* conn) {
+				 char *shared_memory,
+				 struct Arguments *args,
+				 struct tcp_conn *conn)
+{
 	char buffer[1024] = {0};
-	void* shm_buffer = malloc(args->size);
+	void *shm_buffer = malloc(args->size);
 
-
-	atomic_char* guard = (atomic_char*)shared_memory;
+	atomic_char *guard = (atomic_char *)shared_memory;
 	atomic_init(guard, 's');
 	assert(sizeof(atomic_char) == 1);
 
-	for (; args->count > 0; --args->count) {
+	shm_wait(guard);
+
+	send_tcp_packet(conn, TCP_SYN);
+	conn->state = TCP_SYN_SENT;
+
+	shm_notify(guard);
+	shm_wait(guard);
+
+	struct ipv4 *ip;
+	struct tcp *tcp;
+
+	read(descriptor, buffer, sizeof(buffer));
+	ip = buf2ip(buffer);
+	tcp = buf2tcp(buffer, ip);
+	conn->seq = ntohl(tcp->ack);
+	conn->ack = ntohl(tcp->seq) + 1;
+
+	send_tcp_packet(conn, TCP_ACK);
+	conn->state = TCP_ESTABLISHED;
+
+	for (; args->count > 0; --args->count)
+	{
 		shm_wait(guard);
 
 		memcpy(shm_buffer, shared_memory + 1, args->size);
 
-		send_tcp_packet(conn, TCP_SYN);
-		conn->state = TCP_SYN_SENT;
+		// send_tcp_packet(conn, TCP_SYN);
+		// conn->state = TCP_SYN_SENT;
 
-		shm_notify(guard);
-		shm_wait(guard);
+		// shm_notify(guard);
+		// shm_wait(guard);
 
-		struct ipv4* ip;
-		struct tcp* tcp;
+		// struct ipv4 *ip;
+		// struct tcp *tcp;
 
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq) + 1;
+		// read(descriptor, buffer, sizeof(buffer));
+		// ip = buf2ip(buffer);
+		// tcp = buf2tcp(buffer, ip);
+		// conn->seq = ntohl(tcp->ack);
+		// conn->ack = ntohl(tcp->seq) + 1;
 
-		send_tcp_packet(conn, TCP_ACK);
-		conn->state = TCP_ESTABLISHED;
+		// send_tcp_packet(conn, TCP_ACK);
+		// conn->state = TCP_ESTABLISHED;
 
 		conn->seq = ntohl(tcp->ack);
 		conn->ack = ntohl(tcp->seq) + args->size;
@@ -89,44 +114,56 @@ void communicate(int descriptor,
 		// ip = buf2ip(buffer);
 		// tcp = buf2tcp(buffer, ip);
 		// conn->seq = ntohl(tcp->ack);
-		// conn->ack = ntohl(tcp->seq) + args->size;
+		// conn->ack = ntohl(tcp->seq);
 
-		// send_tcp_packet_data(conn, TCP_ACK, args->size);
+		// uint8_t fin_ack_flag = 0;
+		// fin_ack_flag |= TCP_FIN | TCP_ACK;
+		// send_tcp_packet(conn, fin_ack_flag);
 
 		// shm_notify(guard);
 		// shm_wait(guard);
 
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq);
+		// read(descriptor, buffer, sizeof(buffer));
+		// ip = buf2ip(buffer);
+		// tcp = buf2tcp(buffer, ip);
+		// conn->seq = ntohl(tcp->ack);
+		// conn->ack = ntohl(tcp->seq) + 1;
 
-		uint8_t fin_ack_flag = 0;
-		fin_ack_flag |= TCP_FIN | TCP_ACK;
-		send_tcp_packet(conn, fin_ack_flag);
-
-		shm_notify(guard);
-		shm_wait(guard);
-
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq) + 1;
-
-		send_tcp_packet(conn, TCP_ACK);
-		conn->state = TCP_CLOSED;
+		// send_tcp_packet(conn, TCP_ACK);
+		// conn->state = TCP_CLOSED;
 
 		shm_notify(guard);
 	}
 
+	read(descriptor, buffer, sizeof(buffer));
+	ip = buf2ip(buffer);
+	tcp = buf2tcp(buffer, ip);
+	conn->seq = ntohl(tcp->ack);
+	conn->ack = ntohl(tcp->seq);
+
+	uint8_t fin_ack_flag = 0;
+	fin_ack_flag |= TCP_FIN | TCP_ACK;
+	send_tcp_packet(conn, fin_ack_flag);
+
+	shm_notify(guard);
+	shm_wait(guard);
+
+	read(descriptor, buffer, sizeof(buffer));
+	ip = buf2ip(buffer);
+	tcp = buf2tcp(buffer, ip);
+	conn->seq = ntohl(tcp->ack);
+	conn->ack = ntohl(tcp->seq) + 1;
+
+	send_tcp_packet(conn, TCP_ACK);
+	conn->state = TCP_CLOSED;
+
 	cleanup_tcp(descriptor, shm_buffer);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	int segment_id;
-	char* shared_memory;
+	char *shared_memory;
 
 	key_t segment_key;
 
@@ -136,13 +173,15 @@ int main(int argc, char* argv[]) {
 	segment_key = generate_key("shm");
 	segment_id = shmget(segment_key, 1 + args.size, IPC_CREAT | 0666);
 
-	if (segment_id < 0) {
+	if (segment_id < 0)
+	{
 		throw("Could not get segment");
 	}
 
-	shared_memory = (char*)shmat(segment_id, NULL, 0);
+	shared_memory = (char *)shmat(segment_id, NULL, 0);
 
-	if (shared_memory < (char*)0) {
+	if (shared_memory < (char *)0)
+	{
 		throw("Could not attach segment");
 	}
 
--- source/shm/server.c
+++ source/shm/server.c
@@ -21,68 +21,98 @@
 #include "common/sockets.h"
 #include "common/tuntcp.h"
 
-void cleanup_tcp(int descriptor, void* buffer) {
+void cleanup_tcp(int descriptor, void *buffer)
+{
 	close(descriptor);
 	free(buffer);
 }
 
-void cleanup(int segment_id, char* shared_memory) {
+void cleanup(int segment_id, char *shared_memory)
+{
 	shmdt(shared_memory);
 	shmctl(segment_id, IPC_RMID, NULL);
 }
 
-void shm_wait(atomic_char* guard) {
+void shm_wait(atomic_char *guard)
+{
 	while (atomic_load(guard) != 's')
 		;
 }
 
-void shm_notify(atomic_char* guard) {
+void shm_notify(atomic_char *guard)
+{
 	atomic_store(guard, 'c');
 }
 
 void communicate(int descriptor,
-								 char* shared_memory,
-								 struct Arguments* args,
-								 struct tcp_conn* conn) {
+				 char *shared_memory,
+				 struct Arguments *args,
+				 struct tcp_conn *conn)
+{
 	struct Benchmarks bench;
 	char buffer[1024] = {0};
 	int message;
-	atomic_char* guard = (atomic_char*)shared_memory;
+	atomic_char *guard = (atomic_char *)shared_memory;
 
-	void* shm_buffer = malloc(args->size);
+	void *shm_buffer = malloc(args->size);
 
 	// Wait for signal from client
 	shm_wait(guard);
 	setup_benchmarks(&bench);
 
-	for (message = 0; message < args->count; ++message) {
+	conn->state = TCP_LISTEN;
+
+	shm_notify(guard);
+	shm_wait(guard);
+
+	struct ipv4 *ip;
+	struct tcp *tcp;
+
+	read(descriptor, buffer, sizeof(buffer));
+	ip = buf2ip(buffer);
+	tcp = buf2tcp(buffer, ip);
+	conn->state = TCP_SYN_RECEIVED;
+	conn->seq = ntohl(tcp->ack);
+	conn->ack = ntohl(tcp->seq) + 1;
+
+	uint8_t syn_ack_flag = 0;
+	syn_ack_flag |= TCP_SYN | TCP_ACK;
+
+	send_tcp_packet(conn, syn_ack_flag);
+	conn->state = TCP_ESTABLISHED;
+
+	shm_notify(guard);
+	shm_wait(guard);
+
+	for (message = 0; message < args->count; ++message)
+	{
 		bench.single_start = now();
 
 		memset(shared_memory + 1, '*', args->size);
 
-		conn->state = TCP_LISTEN;
+		// conn->state = TCP_LISTEN;
 
 		shm_notify(guard);
 		shm_wait(guard);
 
-		struct ipv4* ip;
-		struct tcp* tcp;
+		// struct ipv4 *ip;
+		// struct tcp *tcp;
 
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->state = TCP_SYN_RECEIVED;
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq) + 1;
+		// read(descriptor, buffer, sizeof(buffer));
+		// ip = buf2ip(buffer);
+		// tcp = buf2tcp(buffer, ip);
+		// conn->state = TCP_SYN_RECEIVED;
+		// conn->seq = ntohl(tcp->ack);
+		// conn->ack = ntohl(tcp->seq) + 1;
 
-		uint8_t syn_ack_flag = 0;
-		syn_ack_flag |= TCP_SYN | TCP_ACK;
+		// uint8_t syn_ack_flag = 0;
+		// syn_ack_flag |= TCP_SYN | TCP_ACK;
 
-		send_tcp_packet(conn, syn_ack_flag);
-		conn->state = TCP_ESTABLISHED;
+		// send_tcp_packet(conn, syn_ack_flag);
+		// conn->state = TCP_ESTABLISHED;
 
-		shm_notify(guard);
-		shm_wait(guard);
+		// shm_notify(guard);
+		// shm_wait(guard);
 
 		read(descriptor, shm_buffer, sizeof(shm_buffer));
 		memcpy(shared_memory + 1, buffer, args->size);
@@ -101,38 +131,46 @@ void communicate(int descriptor,
 		// ip = buf2ip(buffer);
 		// tcp = buf2tcp(buffer, ip);
 		// conn->seq = ntohl(tcp->ack);
-		// conn->ack = ntohl(tcp->seq) + args->size;
+		// conn->ack = ntohl(tcp->seq) + 1;
+
 		// send_tcp_packet(conn, TCP_ACK);
+
+		// uint8_t fin_ack_flag = 0;
+		// fin_ack_flag |= TCP_FIN | TCP_ACK;
+		// send_tcp_packet(conn, fin_ack_flag);
+		// conn->state = TCP_CLOSED;
 
 		// shm_notify(guard);
 		// shm_wait(guard);
 
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq) + 1;
-
-		send_tcp_packet(conn, TCP_ACK);
-
-		uint8_t fin_ack_flag = 0;
-		fin_ack_flag |= TCP_FIN | TCP_ACK;
-		send_tcp_packet(conn, fin_ack_flag);
-		conn->state = TCP_CLOSED;
-
-		shm_notify(guard);
-		shm_wait(guard);
-
 		benchmark(&bench);
 	}
+
+	shm_wait(guard);
+
+	read(descriptor, buffer, sizeof(buffer));
+	ip = buf2ip(buffer);
+	tcp = buf2tcp(buffer, ip);
+	conn->seq = ntohl(tcp->ack);
+	conn->ack = ntohl(tcp->seq) + 1;
+
+	send_tcp_packet(conn, TCP_ACK);
+
+	uint8_t fin_ack_flag = 0;
+	fin_ack_flag |= TCP_FIN | TCP_ACK;
+	send_tcp_packet(conn, fin_ack_flag);
+	conn->state = TCP_CLOSED;
+
+	shm_notify(guard);
 
 	evaluate(&bench, args);
 	cleanup_tcp(descriptor, shm_buffer);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	int segment_id;
-	char* shared_memory;
+	char *shared_memory;
 
 	key_t segment_key;
 
@@ -142,13 +180,15 @@ int main(int argc, char* argv[]) {
 	segment_key = generate_key("shm");
 	segment_id = shmget(segment_key, 1 + args.size, IPC_CREAT | 0666);
 
-	if (segment_id < 0) {
+	if (segment_id < 0)
+	{
 		throw("Error allocating segment");
 	}
 
-	shared_memory = (char*)shmat(segment_id, NULL, 0);
+	shared_memory = (char *)shmat(segment_id, NULL, 0);
 
-	if (shared_memory == (char*)-1) {
+	if (shared_memory == (char *)-1)
+	{
 		throw("Error attaching segment");
 	}
 
