c436e92aa7e7e0331bfe79d01df8b22a651cae1f
Prajval Raval <ravalprajval@gmail.com>
2024-01-12 02:29:56 +0530

    no read

--- source/shm/client.c
+++ source/shm/client.c
@@ -92,11 +92,11 @@ void communicate(int descriptor,
 
 		memcpy(shm_buffer, shared_memory + 1, args->size);
 
-		read(descriptor, buffer, sizeof(buffer));
-		ip = buf2ip(buffer);
-		tcp = buf2tcp(buffer, ip);
-		conn->seq = ntohl(tcp->ack);
-		conn->ack = ntohl(tcp->seq) + args->size;
+		// read(descriptor, buffer, sizeof(buffer));
+		// ip = buf2ip(buffer);
+		// tcp = buf2tcp(buffer, ip);
+		// conn->seq = ntohl(tcp->ack);
+		// conn->ack = ntohl(tcp->seq) + args->size;
 
 		shm_notify(guard);
 	}
